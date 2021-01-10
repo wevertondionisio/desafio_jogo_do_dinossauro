1f3a5f93f0c9bc1925257aef3a42b9eaeb106384
Weverton Dionisio <dionisio.wc@gmail.com>
2021-01-09 22:13:09 -0300

    finalizado

---
+++ _js/app.js
@@ -0,0 +1,78 @@
+const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
+
+let isJumping = false;
+let isGamerOver = false;
+let position = 0;
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if (!isJumping) {
+        jump();
+        }
+    }
+}
+
+function jump() {
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            //Dino descendo
+
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + "px";
+                }
+        }, 20);
+
+        } else {
+            
+            // Dino subindo 
+           
+            position += 20;
+            dino.style.bottom = position + "px";
+        }
+    }, 20);
+}
+
+function createCactus() {
+    const cactus = document.createElement("div");
+    let cactusPosition = 2000;
+    let randomTime = Math.random() * 6000;
+    
+    if (isGamerOver) return;
+
+    cactus.classList.add("cactus");
+    background.appendChild(cactus);
+    cactus.style.left = cactusPosition + "px";
+
+    let leftInternal = setInterval(() => {
+        
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) { 
+            
+            // Game Over!
+            clearInterval(leftInternal);
+            isGamerOver = true;
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo!</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + "px";
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener("keyup", handleKeyUp);
+
